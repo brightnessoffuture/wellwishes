dffe41577c7abbb0b81ae60db1ee246526614377
Wong Chong Hao <wongchonghao@Wongs-MacBook-Air.local>
2023-10-08 18:13:55 +0800

    Prepare for Heroku deployment

--- server.js
+++ server.js
@@ -23,3 +23,8 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
