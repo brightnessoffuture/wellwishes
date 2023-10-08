0d232155d6889ec57d78a02b1c1843ca93e97857
Wong Chong Hao <wongchonghao@Wongs-MacBook-Air.local>
2023-10-08 19:55:02 +0800

    Updated server configuration for Heroku

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('new message', (msg) => {
-        io.emit('new message', msg);
-    });
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-});
-
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
